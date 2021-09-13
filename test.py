e9748a96e58e47f90d1a8707e338fb8c2cae282f
Agelaxel <90190555+Agelaxel@users.noreply.github.com>
2021-09-13 13:18:58 +0200

    hggygh

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
 print("Hej på er EE20")
 name = input("vad heter du?")
 print(f"hej på dig agle")
+
 age = input("hur gammal är du?")
 print(f"oj så hur gammal du är, hela 69 år.")
+
+Tal1 = 12
+tal2 = 12
+
+Summa = Tal1 * tal2
+print(f"Summan blir {Summa}")
